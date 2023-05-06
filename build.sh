a48cc88bb5fdaada7da3760813010e074bc19d54
Grzegorz Uriasz <gorbak25@gmail.com>
2023-05-06 23:17:42 +0200

    Strip

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 docker build -t hocus-linux-kernel .
 id=$(docker create hocus-linux-kernel /)
-docker cp $id:/vmlinux - | tar x -O > vmlinux
+docker cp $id:/vmlinux - | tar x -O > vmlinux-6.2-x86_64.bin
 docker rm -v $id
+strip --strip-all vmlinux-6.2-x86_64.bin
